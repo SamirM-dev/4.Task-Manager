82162c9420052cbb186d3a5e2e513ee698a334c9
SamirM-dev <memmedovsamirws@gmail.com>
2026-07-03 15:46:53 +0400

    clear final project

---
+++ src/main/java/com/example/bootfinal/BootfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bootfinal/BootfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
